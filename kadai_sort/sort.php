0309ba16abd4446864b3bc4d036cdcc55694c7f3
k-moriya <ppp32961@gmail.com>
2024-01-08 12:23:53 +0900

    関数の引数を2つに修正。if文の条件式を修正

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,24 +9,23 @@
 <body>
     <p>
         <?php
-        function sort_2way($order){
-            $nums = [15, 4, 18, 23, 10 ];
-            if($order == TRUE){
+        
+        function sort_2way($array,$order){
+            
+            if($order){
                 echo '昇順にソートします。<br>';
-                sort($nums);
-                foreach($nums as $array){
-                    echo $array . '<br>';
-                }
+                sort($array);
             }else{
                 echo '降順にソートします。<br>';
-                rsort($nums);
-                foreach($nums as $array){
-                    echo $array . '<br>';
-                }
+                rsort($array);
+            }
+            foreach($array as $num){
+                echo $num . '<br>';
             }
         }
-        sort_2way(TRUE);
-        sort_2way(FALSE);
+        $nums = [15, 4, 18, 23, 10 ];
+        sort_2way($nums,TRUE);
+        sort_2way($nums,FALSE);
         ?>
     </p>
 </body>
